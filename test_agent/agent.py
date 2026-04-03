c879c2067ec106118d13d858138c9861cedcb77a
rosawant <roshansawant700@gmail.com>
2026-04-03 12:18:12 +0530

    Fixed typo

--- test_agent/agent.py
+++ test_agent/agent.py
@@ -1,11 +1,11 @@
-from google.adk import Agent
+from google.adk.agents import Agent
 
 root_agent = Agent(
     name="test_agent",
     description="A test agent for demonstration purposes.",
     model="gemini-2.0-flash",
-    instructions="""
+    instruction="""
     You are a helpful assistant that provides information and answers questions to the best of your ability.
     Ask user his/her name and greet them. Then ask how you can assist them today.
-    """
+    """,
 )
