af460ae35df9309797bb330834c8cbc404464110
rosawant <roshansawant700@gmail.com>
2026-04-03 17:39:55 +0530

    adde dadjokes

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Google ADK
 
 
+
 ## limitations:
 1. Agent directory name and the agent name must be same
 2. Agent name must be a valid identifier. It should start with a letter (a-z, A-Z) or an underscore (_), and can only contain letters, digits (0-9), and underscores
 3. Google prebuilt tools cant be combined with the custom tools
+4. Use models which supports tools [Ollama Tool Supporting Models](https://ollama.com/search?c=tools)
--- dad_joketool_agent/agent.py
+++ dad_joketool_agent/agent.py
@@ -1,21 +1,28 @@
 from google.adk.agents import Agent
 from google.adk.models.lite_llm import LiteLlm
+import random
 
 
 def dad_joke_tool() -> dict:
     """Returns a random dad joke."""
-    return {
-        "joke": "Why don't scientists trust atoms? Because they make up everything!"
-    }
+    jokes = [
+        "Why don't scientists trust atoms? Because they make up everything!",
+        "Why did the scarecrow win an award? Because he was outstanding in his field!",
+        "Why don't skeletons fight each other? They don't have the guts.",
+        "What do you call fake spaghetti? An impasta!",
+        "Why did the bicycle fall over? Because it was two-tired!"
+    ]
+    return {"joke": random.choice(jokes)}
 
 root_agent = Agent(
-    model=LiteLlm(model="ollama_chat/gemma:2b"),
+    model=LiteLlm(model="ollama_chat/qwen3.5:0.8b"),
     name="dad_joketool_agent",
     description=(
         """This agent tells dad jokes using a custom tool. It uses the LiteLlm model for generating responses."""
     ),
     instruction="""
-      You roll dice and answer questions about the outcome of the dice rolls.
+    You are a helpful assistant that tells dad jokes. Use the 'dad_joke_tool' to get a random dad joke and share it with the user.
+    - dad_joke_tool: A tool that returns a random dad joke when called.
     """,
     tools=[
         dad_joke_tool,
